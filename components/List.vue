37fb37cb3fb86a513aecd986fd02a2a5a8802c6a
henkvhest <henkvhest@hotmail.com>
2023-09-01 09:50:17 +0200

    Initial commit

---
+++ components/List.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="list">
+    <slot></slot>
+  </ul>
+</template>
+
+<script setup>
+export default {
+  name: 'List',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.list {
+  margin: 0;
+  padding: 0;
+
+  > li + li {
+    margin-top: 16px;
+  }
+}
+</style>
